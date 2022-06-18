7ac327555aa969cee18aef7ae3e16c600851112b
leejw79 <leejw79@gmail.com>
2022-06-19 00:19:08 +0900

    setup rds

--- backend/routes/car.js
+++ backend/routes/car.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
+require('dotenv').config();
+
 const connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'carhacker',
-    password : 'qhdkswp1!@',
-    database : 'car_db'
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    port : process.env.DB_PORT,
+    database : process.env.DB_NAME
 });
 
 connection.connect();
