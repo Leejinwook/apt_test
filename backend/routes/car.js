0385eb2f55e1ec613dd5d6593a2f29a1b60a533e
leejw79 <leejw79@gmail.com>
2022-06-20 03:12:18 +0900

    add func

--- backend/routes/car.js
+++ backend/routes/car.js
@@ -58,7 +58,6 @@ router.get('/history', function(req, res, next){
         }else{
             if (result && result.length > 0){
                 console.log(result.length);
-                //res.json({'status':'OK'});
                 res.render("carinfo.ejs", {
                     'carid': result[0].carid,
                     'carnumber': result[0].carnumber,
@@ -70,6 +69,80 @@ router.get('/history', function(req, res, next){
                     'carnumber': "",
                     'title': ""
                 });
+            }
+        }
+    });
+});
+
+router.post('/enginStatusReq', function(req, res, next){
+    console.log(req.body);
+    const engin = ["STOP", "START", "STOP_REQ", "START_REQ"]
+    const carid = req.body.carid;
+    const engin_status = req.body.engin_status.toUpperCase();
+    var req_ok = false;
+    // 요청 정합성 확인
+    engin.includes(engin_status)? req_ok = true: req_ok=false;
+
+    // 현재 상태와 요청상태 비교
+    if(req_ok){
+        var sql = "SELECT engin_status FROM car_engin WHERE carid = ? order by id desc limit 1;"
+        var value = [carid];
+        var cur_engin_status = "";
+        connection.query(sql, value, function(err, result, field){
+            if(err){
+                console.log(err);
+                res.json({'status':'DB ERROR'});
+            }else{
+                console.log(result);
+                if (result && result.length > 0){
+                    cur_engin_status = result[0].engin_status;
+                }else{
+                    cur_engin_status = "";
+                }
+            }
+
+            if(cur_engin_status == "" || cur_engin_status.replace('_REQ', '') != engin_status.replace('_REQ', '')){
+                if(engin.includes(engin_status)){
+                    var sql = "INSERT INTO car_engin (carid, engin_status) values (?,?)";
+                    var values = [carid, engin_status];
+                    connection.query(sql, values, function(err, result, field){
+                        if(err){
+                            console.log(err);
+                            res.json({'status':'DB ERROR'});
+                        }else{
+                            console.log(result);
+                            res.json({'status': engin_status});
+                        }
+                    });
+                    // To-Do : 자동차(라즈베리파이)로 엔진시작 요청 구현
+                }else{
+                    res.json({'status':'DB ERROR'})
+                }
+            }else{
+                res.json({'status':'REQUEST STATUS ERROR(SAME)'})
+            }
+        });
+    }
+});
+
+// 엔진 현재상태 조회
+router.post('/enginStatus', function(req, res, next){
+    console.log(req.body);
+    const carid = req.body.carid;
+
+    var sql = "SELECT engin_status FROM car_engin WHERE carid = ? order by id desc limit 1;"
+    var value = [carid];
+    var cur_engin_status = "";
+    connection.query(sql, value, function(err, result, field){
+        if(err){
+            console.log(err);
+            res.json({'status':'DB ERROR'});
+        }else{
+            console.log(result);
+            if (result && result.length > 0){
+                res.json({'status': result[0].engin_status});
+            }else{
+                res.json({'status':'Engin Stop'});
             }
         }
     });
--- setting/setup_db.sql
+++ setting/setup_db.sql
@@ -15,4 +15,16 @@ create table if not exists car_info(
     time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     primary key(id)
 );
+
+create table if not exists car_engin(
+    id int not null AUTO_INCREMENT,
+    carid varchar(50) NOT NULL,
+    engin_status varchar(20) NOT NULL DEFAULT "STOP",
+    time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    primary key(id)
+);
+
 create table if not exists car_info(id int not null AUTO_INCREMENT, carid varchar(50) NOT NULL, carnumber varchar(50) NOT NULL,speed int,latitude varchar(50),longitude varchar(50),time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,primary key(id));
+create table if not exists car_engin(id int not null AUTO_INCREMENT, carid varchar(50) NOT NULL, engin_status varchar(20) NOT NULL DEFAULT "STOP", time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, primary key(id));
+
+select engin_status from car_engin where carid = "09e7b07a-eeb6-11ec-acf2-acde48001122" and engin_status = "START_REQ" order by id desc limit 1;
